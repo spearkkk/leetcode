d86e4d4aeb4e2c175650759d9d79d69a6e06132f
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-15 20:52:49 +0900

    Time: 240 ms (44.89%), Space: 35.1 MB (36.99%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        if (needle.isEmpty()) {
+            return 0
+        }
+        return haystack.indexOf(needle)
+    }
+}
