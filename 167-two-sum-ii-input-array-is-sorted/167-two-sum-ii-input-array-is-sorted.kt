7af2d7010f6e201e9325db9b8c748be7fb5d5c5f
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-17 20:41:13 +0900

    Time: 1069 ms (8.60%), Space: 47.4 MB (58.28%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.kt
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.kt
@@ -1,9 +1,14 @@
 class Solution {
     fun twoSum(numbers: IntArray, target: Int): IntArray {
         for (cursor in numbers.indices) {
+            val nextTarget = target - numbers[cursor]
+            
             for (secondCursor in cursor + 1 until numbers.size) {
-                if (numbers[cursor] + numbers[secondCursor] == target) {
+                if (numbers[secondCursor] == nextTarget) {
                     return intArrayOf(cursor + 1, secondCursor + 1)
+                }
+                if (numbers[secondCursor] > nextTarget) {
+                    break
                 }
             }
         }
