dd62306198623bfa430baa813c6be7d9a9ff7a8c
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-19 00:16:52 +0900

    Time: 265 ms (85.85%), Space: 41 MB (62.26%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun thirdMax(nums: IntArray): Int {
+        val holder = hashSetOf<Int>()
+        var first: Long = Int.MIN_VALUE.toLong() - 1
+        var second: Long = Int.MIN_VALUE.toLong() - 1
+        var third: Long = Int.MIN_VALUE.toLong() - 1
+
+        for (cur in nums) {
+            if (holder.contains(cur)) {
+                continue
+            }
+            holder.add(cur)
+
+            if (cur >= first) {
+                third = second
+                second = first
+                first = cur.toLong()
+            } else if (cur >= second) {
+                third = second
+                second = cur.toLong()
+            } else if (cur > third) {
+                third = cur.toLong()
+            }
+        }
+        return if (third == Int.MIN_VALUE.toLong() - 1) first.toInt() else third.toInt()
+    }
+}
