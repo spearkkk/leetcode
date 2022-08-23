6c0b75c990a02e9f22923e65b1ba1da291f11c65
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-23 21:19:43 +0900

    Time: 367 ms (38.72%), Space: 43.5 MB (53.81%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.kt
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.kt
@@ -6,6 +6,7 @@ class Solution {
 
         val middle = (end - start) / 2 + start
 
+        /// 앞으로 가야한다.
         if (nums[middle] == target) {
             return searchFirst(nums, target, start, middle - 1, middle)
         }
@@ -23,6 +24,7 @@ class Solution {
 
         val middle = (end - start) / 2 + start
 
+        /// 뒤로 가야한다.
         if (nums[middle] == target) {
             return searchLast(nums, target, middle + 1, end, middle)
         }
@@ -34,11 +36,16 @@ class Solution {
     }
 
     fun searchRange(nums: IntArray, target: Int): IntArray {
+        /// first를 먼저 찾는다.
         val first = searchFirst(nums, target, 0, nums.size - 1, -1)
+        /// first를 찾은 이후부터 last를 찾는다.
         val last = searchLast(nums, target, first + 1, nums.size - 1, -1)
+        
+        /// 엣지 케이스로 first는 찾았지만 last는 못찾은 경우, 즉 원소가 한개일 경우
         if (last == -1 && first != last) {
             return intArrayOf(first, first)
         }
+        
         return intArrayOf(first, last)
     }
 }
