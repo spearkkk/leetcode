6e664cfc1539a09107d796af4ee699807b364076
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-24 22:50:51 +0900

    Time: 405 ms (53.00%), Space: 43.3 MB (35.00%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.kt
+++ 40-combination-sum-ii/40-combination-sum-ii.kt
@@ -7,13 +7,19 @@ class Solution {
         }
 
 
+        /// 현재 값인 start 부터 끝까지 더해본다.
         for (value in start until counts.size) {
             val maximumCount = counts[value]
 
+            /// 더할 때에는 가지고 있는 개수를 활용한다.
+            /// 0개를 사용하거나 maximum까지 사용한다.
             for (count in 1 .. maximumCount) {
+                /// 실제 사용하기 전에 유효성을 검증을 먼저 한다.
                 if (value * count + currentSum > target) {
                     continue
                 }
+                
+                /// 레퍼런스이기 때문에 복사해서 넘겨준다.
                 val copied = selected.toMutableList()
                 copied.addAll((1 .. count).map { value })
                 sum(counts, value + 1, target, copied, result)
@@ -23,11 +29,14 @@ class Solution {
 
 
     fun combinationSum2(candidates: IntArray, target: Int): List<List<Int>> {
+        /// candidates에 나오는 수에 대한 개수를 저장한다.
         val counts = IntArray(51) { 0 }
         for (candidate in candidates) {
             counts[candidate] += 1
         }
+        
         val result = mutableSetOf<List<Int>>()
+        /// 개수를 활용하여 target을 구한다.
         sum(counts, 0, target, mutableListOf(), result)
         return result.toList()
     }
