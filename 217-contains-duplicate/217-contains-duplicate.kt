ce5a0a7b8a995a0e4831bbce9dc4d20dd1a0b972
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-25 15:43:07 +0900

    Time: 748 ms (48.93%), Space: 71.1 MB (65.98%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val holder = hashSetOf<Int>()
+
+        for (i in nums) {
+            if (holder.contains(i)) {
+                return true
+            }
+            holder.add(i)
+        }
+        return false
+    }
+}
