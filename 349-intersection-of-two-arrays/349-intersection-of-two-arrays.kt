af71205d47e95198af9c20a3898f32cc2dde577d
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-25 15:50:03 +0900

    Time: 389 ms (24.81%), Space: 42.8 MB (13.53%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val marked = BooleanArray(1001) { false }
+
+        for (num in nums1) {
+            if (nums2.contains(num)) {
+                marked[num] = true
+            }
+        }
+
+        val holder = mutableListOf<Int>()
+        for ((i, v) in marked.withIndex()) {
+            if (v) {holder.add(i)}
+        }
+
+        return holder.toIntArray()
+    }
+}
