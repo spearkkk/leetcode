b8e6b0caa2306649b1559ce5b3224d21c1144299
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-25 15:48:57 +0900

    Time: 2772 ms (5.16%), Space: 716.2 MB (5.01%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.kt
@@ -0,0 +1,20 @@
+class Solution {
+    private fun shift(zeroPosition: Int, lastPosition: Int, nums: IntArray): Unit {
+        for (i in zeroPosition until lastPosition) {
+            nums[i] = nums[i + 1]
+        }
+        nums[lastPosition] = 0
+    }
+    fun moveZeroes(nums: IntArray) {
+        for (i in 1..nums.size) {
+            val currentPosition = nums.size - i
+
+            val zeroPosition = nums.slice(0 until currentPosition).lastIndexOf(0)
+            if (zeroPosition == -1) {
+                break
+            }
+            shift(zeroPosition, currentPosition, nums)
+
+        }
+    }
+}
