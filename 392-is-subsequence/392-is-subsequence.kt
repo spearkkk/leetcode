4ccb495434d0f69877d1657910eaaefa4226bde4
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-25 15:50:30 +0900

    Time: 272 ms (16.36%), Space: 35.7 MB (15.99%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun isSubsequence(s: String, t: String): Boolean {
+        val holder = s.toMutableList()
+        for (cur in t) {
+            if (holder.isEmpty()) {
+                return true
+            }
+            if (holder[0] == cur) {
+                holder.removeAt(0)
+            }
+        }
+
+        return holder.isEmpty()
+    }
+}
