6184bacab708642f98a48b0311066941552e829a
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-25 21:16:57 +0900

    Time: 382 ms (46.74%), Space: 45.5 MB (18.21%) - LeetHub

--- 46-permutations/46-permutations.kt
+++ 46-permutations/46-permutations.kt
@@ -6,10 +6,15 @@ class Solution {
         }
 
         val nextResult = mutableListOf<List<Int>>()
+        
+        /// 각 값이 맨 처음에 나올 경우를 다 계산한다.
         for (cursor in nums.indices) {
             val currentValue = nums[cursor]
+            
+            /// 나머지 배열의 값들로 재귀 반복
             val currentResult = permute2(nums.withIndex().filter { it.index != cursor }.map { it.value }.toIntArray())
 
+            /// 위에서 구한 결과에 맨 앞에 위의 값을 넣는다.
             for (i in currentResult) {
                 val last = i.toMutableList()
                 last.add(0, currentValue)
@@ -21,6 +26,7 @@ class Solution {
         return nextResult
     }
     fun permute(nums: IntArray): List<List<Int>> {
+        /// 엣지 케이스
         if (nums.size < 2) {
             return listOf(nums.toList())
         }
