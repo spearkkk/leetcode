ff15e6ec2a1ed5919d73ecb4c279b213cdba0f51
ChangKwon Jeong <spearkkp@gmail.com>
2022-08-27 09:32:31 +0900

    Time: 325 ms (86.67%), Space: 43.4 MB (88.89%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.kt
@@ -0,0 +1,44 @@
+class Solution {
+    private fun permute2(nums: IntArray): List<List<Int>> {
+        if (nums.size == 2) {
+            if (nums[0] == nums[1]) {
+                return listOf(listOf(nums[0], nums[1]))
+            }
+            return listOf(listOf(nums[0], nums[1]), listOf(nums[1], nums[0]))
+        }
+
+        val nextResult = mutableListOf<List<Int>>()
+
+        val footprint = mutableSetOf<Int>()
+
+        /// 각 값이 맨 처음에 나올 경우를 다 계산한다.
+        for (cursor in nums.indices) {
+            val currentValue = nums[cursor]
+
+            if (footprint.contains(currentValue)) {
+                continue
+            }
+
+            /// 나머지 배열의 값들로 재귀 반복
+            val currentResult = permute2(nums.withIndex().filter { it.index != cursor }.map { it.value }.toIntArray())
+
+            /// 위에서 구한 결과에 맨 앞에 위의 값을 넣는다.
+            for (i in currentResult) {
+                val last = i.toMutableList()
+                last.add(0, currentValue)
+                nextResult.add(last)
+            }
+            footprint.add(currentValue)
+        }
+
+
+        return nextResult
+    }
+    fun permuteUnique(nums: IntArray): List<List<Int>> {
+        /// 엣지 케이스
+        if (nums.size < 2) {
+            return listOf(nums.toList())
+        }
+        return permute2(nums)
+    }
+}
