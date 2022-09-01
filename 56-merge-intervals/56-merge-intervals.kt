eba49a70233f883e20e3fde4f02b2ecfdbd747ae
ChangKwon Jeong <spearkkp@gmail.com>
2022-09-01 10:12:27 +0900

    Time: 360 ms (93.27%), Space: 43 MB (99.33%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun merge(intervals: Array<IntArray>): Array<IntArray> {
+        val footprint = IntArray(10000 + 1) { -1 }
+
+        for (interval in intervals) {
+            if (footprint[interval[0]] < interval[1]) {
+                footprint[interval[0]] = interval[1]
+            }
+        }
+
+        val result = mutableListOf<IntArray>()
+
+        var cursor = 0
+        while (cursor < footprint.size) {
+            if (footprint[cursor] != -1) {
+                val start = cursor
+                var lastIndex = footprint[cursor]
+
+                while (cursor <= lastIndex) {
+                    if (footprint[cursor] != -1 && footprint[cursor] > lastIndex) {
+                        lastIndex = footprint[cursor]
+                    }
+                    cursor += 1
+                }
+
+                result.add(intArrayOf(start, lastIndex))
+            } else {
+                cursor += 1
+            }
+        }
+
+        return result.toTypedArray()
+    }
+}
