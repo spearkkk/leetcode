b43d62935b15d31efc63f74d8089207ad60d6a93
ChangKwon Jeong <spearkkp@gmail.com>
2022-09-07 09:08:44 +0900

    Time: 286 ms (51.46%), Space: 35.7 MB (79.61%) - LeetHub

--- 61-rotate-list/61-rotate-list.kt
+++ 61-rotate-list/61-rotate-list.kt
@@ -1,21 +1,17 @@
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
 class Solution {
     fun rotateRight(head: ListNode?, k: Int): ListNode? {
+        /// 엣지 케이스
         if (head?.next == null) {
             return head
         }
 
+        /// head 노드를 이미 포함한 상황
         var cnt = 1
-        var last: ListNode?
+        
+        /// 맨 마지막 노드를 기억했다가 나중에 연결해줄 예정
+        val last: ListNode?
 
+        /// 개수와 맨 마지막 노드 구하기
         var current: ListNode = head
         while (current.next != null) {
             current = current.next!!
@@ -23,15 +19,19 @@ class Solution {
         }
         last = current
 
+        /// k 수 최적화, 최적화 이후 0이라면 할 일이 없기 때문에 바로 반환
         val n = k % cnt
         if (n == 0) {
             return head
         }
+        
+        /// n(k)만큼 위치를 이동 다만, 이미 head가 1을 포함하고 있는 점을 감안
         var newLast: ListNode = head
         for (ignored in 1 until (cnt - n)) {
             newLast = newLast.next!!
         }
 
+        /// 연결해주기
         val newHead = newLast.next
         newLast.next = null
         last.next = head
