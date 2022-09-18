7e24fbea2217b6685d26439465f2f2d16c83e7dd
ChangKwon Jeong <spearkkp@gmail.com>
2022-09-19 08:34:02 +0900

    Time: 460 ms (14.29%), Space: 46.9 MB (6.35%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.kt
@@ -0,0 +1,25 @@
+class Solution {
+    private fun doConnect(stack: MutableList<Node>) {
+        if (stack.isEmpty()) {
+            return
+        }
+        
+        val nextStack = mutableListOf<Node>()
+        var cursor: Node? = null
+        while (stack.isNotEmpty()) {
+            val nextNode = stack.removeAt(0)
+            
+            if (nextNode.left != null) nextStack.add(nextNode.left!!)
+            if (nextNode.right != null) nextStack.add(nextNode.right!!)
+            
+            if (cursor != null) cursor.next = nextNode
+            cursor = nextNode
+        }
+        doConnect(nextStack)
+    }
+    
+    fun connect(root: Node?): Node? {
+        if (root != null) doConnect(mutableListOf(root))
+        return root
+    }
+}
