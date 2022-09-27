509f1f6c9233ec6eab0de29fd419566a63910dc5
ChangKwon Jeong <spearkkp@gmail.com>
2022-09-28 00:28:41 +0900

    Time: 203 ms (100.00%), Space: 35.6 MB (97.22%) - LeetHub

---
+++ 147-insertion-sort-list/147-insertion-sort-list.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun insertionSortList(head: ListNode?): ListNode? {
+        val newHead = ListNode(-5001)
+        newHead.next = head
+
+        var cur: ListNode = newHead
+
+        while (cur.next != null) {
+            if (cur.`val` <= cur.next!!.`val`) {
+                cur = cur.next!!
+                continue
+            }
+
+            var anotherCur: ListNode = newHead
+            while (anotherCur.next!! != cur && anotherCur.next!!.`val` < cur.next!!.`val`) {
+                anotherCur = anotherCur.next!!
+            }
+            val movingNode = cur.next!!
+            cur.next = cur.next!!.next
+
+            movingNode.next = anotherCur.next!!
+            anotherCur.next = movingNode
+        }
+        return newHead.next
+    }
+}
