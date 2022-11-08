fd87b1f003943430e112304fbc7555834cfdfc2f
ChangKwon Jeong <spearkkp@gmail.com>
2022-11-08 09:28:37 +0900

    Time: 218 ms (93.33%), Space: 37.9 MB (6.67%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.kt
+++ 1544-make-the-string-great/1544-make-the-string-great.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun makeGood(s: String): String {
-        fun make(str: String, result: String, cursor: Int): String {
+        fun make(str: String, result: StringBuilder, cursor: Int): StringBuilder {
             if (cursor >= str.length) {
                 return result
             }
@@ -10,12 +10,12 @@ class Solution {
                 val right = str[cursor]
                 val gap = left.toInt() - right.toInt()
                 if (gap == 32 || gap == -32) {
-                    return make(str, result.slice(0 until result.length - 1), cursor + 1)
+                    return make(str, result.deleteCharAt(result.length - 1), cursor + 1)
                 }
             }
-            return make(str, result + str[cursor], cursor + 1)
+            return make(str, result.append(str[cursor]), cursor + 1)
         }
 
-        return make(s, "", 0)
+        return make(s, StringBuilder(), 0).toString()
     }
 }
