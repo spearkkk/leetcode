b0994c80ed2052655382711f2fc9b20d185a7083
ChangKwon Jeong <spearkkp@gmail.com>
2022-11-12 21:25:34 +0900

    Time: 263 ms (50.00%), Space: 32.8 MB (100.00%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.kt
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.kt
@@ -1,22 +1,42 @@
 class Solution {
     fun countNumbersWithUniqueDigits(n: Int): Int {
-        if (n == 0) return 1
-        
-        var uniqueCnt = 9
-        var available = 9
-        
-        var count = 10
-        
-        var cursor = 1
-        while (cursor < n && available > 0) {
-            uniqueCnt *= available
-            
-           count += uniqueCnt
-           available -= 1 
-            
-            cursor += 1
+        if (n > 10) {
+            return countNumbersWithUniqueDigits(10)
         }
 
+        val mark = BooleanArray(10) { false }
+
+        fun count(num: Int, max: Int): Int {
+            var count = 0
+            if (num < max) {
+                count += 1
+            } else {
+                return count
+            }
+
+            for (cursor in 0 .. 9) {
+                if (mark[cursor]) {
+                    continue
+                }
+
+                mark[cursor] = true
+                count += count(num * 10 + cursor, max)
+                mark[cursor] = false
+            }
+            return count
+        }
+
+        var max = 1
+        for (ignore in 1 .. n) {
+            max *= 10
+        }
+        var count = 1
+
+        for (cursor in 1 .. 9) {
+            mark[cursor] = true
+            count += count(cursor, max)
+            mark[cursor] = false
+        }
         return count
     }
 }
