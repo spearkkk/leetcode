ec773602b264fefea4e21ff4dfdc8186e5cfc491
ChangKwon Jeong <spearkkp@gmail.com>
2022-11-13 16:57:48 +0900

    Time: 891 ms (5.30%), Space: 96.9 MB (8.72%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.kt
@@ -0,0 +1,40 @@
+class Solution {
+    fun increasingTriplet(nums: IntArray): Boolean {
+        val dp = IntArray(nums.size) { Int.MIN_VALUE }
+        var cnt = 0
+
+        fun bSearch(target: Int): Int {
+            var start = 0
+            var end = cnt
+            var index = 0
+
+            while (start <= end) {
+                val mid = (start + end) / 2
+
+                if (target <= dp[mid]) {
+                    index = mid
+                    end = mid - 1
+                } else {
+                    start = mid + 1
+                }
+            }
+
+            if (start >= cnt) {
+                return -1
+            }
+            return index
+        }
+
+        for (cursor in nums.indices) {
+            val index = bSearch(nums[cursor])
+            if (index == -1) {
+                dp[cnt] = nums[cursor]
+                cnt += 1
+            } else {
+                dp[index] = nums[cursor]
+            }
+        }
+
+        return cnt >= 3
+    }
+}
