a06024866353631201367a22d136fdce63e48b5f
ChangKwon Jeong <spearkkp@gmail.com>
2023-01-10 21:21:40 +0900

    Time: 241 ms (65.34%), Space: 36.9 MB (50.57%) - LeetHub

--- 0139-word-break/0139-word-break.kt
+++ 0139-word-break/0139-word-break.kt
@@ -1,29 +1,49 @@
 class Solution {
+    //dp, top-down
+//    fun wordBreak(s: String, wordDict: List<String>): Boolean { 
+//        val n = s.length
+//        val words = wordDict.toSet()
+//        val memo = mutableMapOf<Int, Boolean>()
+//
+//        fun dfs(leftCursor: Int): Boolean {
+//            if (leftCursor == n) {
+//                return true
+//            }
+//
+//            if (memo[leftCursor] != null) {
+//                return memo[leftCursor]!!
+//            }
+//
+//            for (rightCursor in leftCursor .. n) {
+//                val left = s.slice(leftCursor until rightCursor)
+//                if (words.contains(left) && dfs(rightCursor)) {
+//                    memo[leftCursor] = true
+//                    return true
+//                }
+//            }
+//            memo[leftCursor] = false
+//            return false
+//        }
+//
+//        return dfs(0)
+//    }
+
+    // dp, bottom-up
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
         val n = s.length
         val words = wordDict.toSet()
-        val memo = mutableMapOf<Int, Boolean>()
+        val dp = BooleanArray(n + 1) { false }
+        dp[0] = true
 
-        fun dfs(leftCursor: Int): Boolean {
-            if (leftCursor == n) {
-                return true
-            }
+        for (leftCursor in 0 until n) {
+            for (rightCursor in leftCursor + 1 .. n) {
+                val sub = s.slice(leftCursor until rightCursor)
 
-            if (memo[leftCursor] != null) {
-                return memo[leftCursor]!!
-            }
-
-            for (rightCursor in leftCursor .. n) {
-                val left = s.slice(leftCursor until rightCursor)
-                if (words.contains(left) && dfs(rightCursor)) {
-                    memo[leftCursor] = true
-                    return true
+                if (words.contains(sub) && dp[leftCursor]) {
+                    dp[rightCursor] = true
                 }
             }
-            memo[leftCursor] = false
-            return false
         }
-
-        return dfs(0)
+        return dp[n]
     }
 }
