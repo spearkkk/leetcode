57afe5a87b56354e4a1fee45263e879c1284797b
ChangKwon Jeong <spearkkp@gmail.com>
2023-07-04 20:28:23 +0900

    Time: 201 ms (40.00%), Space: 36.9 MB (100.00%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.kt
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun removeDuplicateLetters(s: String): String {
+        // 문자를 카운트한다.
         val charToCount = HashMap<Char, Int>()
 
         for (letter in s) {
@@ -9,6 +10,7 @@ class Solution {
         val result = ArrayList<Char>()
 
         for (letter in s) {
+            // 처리할 문자에 대한 카운트를 감소시킨다.
             charToCount[letter] = charToCount[letter]!! - 1
 
             if (result.isEmpty()) {
@@ -16,14 +18,16 @@ class Solution {
                 continue
             }
 
+            // result 값은 최적의 상태이기 때문에 중복으로 추가할 필요가 없다.
             if (result.contains(letter)) {
                 continue
             }
-            
+
+            // 새로운 문자를 추가하기 전, 순서를 고려하며 문자열을 추가한다. 
             while (result.isNotEmpty() && result.last() > letter && charToCount[result.last()]!! > 0) {
                 result.removeAt(result.size - 1)
             }
-            
+
             result.add(letter)
         }
 
