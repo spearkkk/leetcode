920707f5b53369bc64f0cb026f31a008d9536ad6
ChangKwon Jeong <spearkkp@gmail.com>
2023-07-07 00:11:51 +0900

    Time: 265 ms (46.97%), Space: 41.9 MB (31.82%) - LeetHub

--- 0135-candy/0135-candy.kt
+++ 0135-candy/0135-candy.kt
@@ -4,7 +4,11 @@ class Solution {
             return counts
         }
 
-        var flag = 0
+        // 이웃에 대한 상태를 마킹한다.
+        // -1: 이웃이 같은 값일 때
+        // 0: 왼쪽 이웃이 오른쪽보다 더 클 때
+        // 1: 왼쪽 이웃이 오른쪽보다 더 작을 때
+        val flag: Int
         val newCount = if (ratings[index - 1] == ratings[index]) {
             flag = -1
             1
@@ -16,8 +20,11 @@ class Solution {
             count + 1
         }
         counts[index] = newCount
+        
+        // 왼쪽에서 오른쪽으로 가면서 사탕을 나눠준다.
         val tmp =  dig(index + 1, newCount, ratings, counts)
 
+        // 위의 결과에서 왼쪽이웃이 오른쪽보다 더 클 때에는 사탕을 하나라도 더 가져야 하기 때문에 이를 맞춰준다.
         if (flag == 0 && counts[index - 1] <= counts[index]) {
             counts[index - 1] = counts[index] + 1
         }
