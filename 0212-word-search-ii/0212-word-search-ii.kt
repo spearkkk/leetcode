fa3336943b557abe88e4c08291903205ff2f891a
ChangKwon Jeong <spearkkp@gmail.com>
2023-12-17 14:51:57 +0900

    Time: 299 ms (98.67%), Space: 39.7 MB (90.67%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.kt
+++ 0212-word-search-ii/0212-word-search-ii.kt
@@ -1,58 +1,70 @@
 class Solution {
     fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
+        data class TrieNode(var word: String = "", val children: MutableMap<Char, TrieNode> = mutableMapOf())
 
-        fun exist(board: Array<CharArray>, word: String): Boolean {
-            val n = board.size
-            val m = board[0].size
-    
-            fun bt(cursor: Int, y: Int, x: Int): Boolean {
-                if (cursor >= word.length) {
-                    return true
-                }
-    
-                // 현재 좌표를 체크한다.
-                if (y < 0 || y >= n || x < 0 || x >= m) {
-                    return false
-                }
-    
-                if (board[y][x] != word[cursor]) {
-                    return false
-                }
-    
-                // 사용했는지 마킹을 다른 배열이 아니라 문자를 바꾸어서 마킹한다.
-                val tmp = board[y][x]
-                board[y][x] = '*'
-    
-                val result = bt(cursor + 1, y + 1, x)
-                    || bt(cursor + 1, y - 1, x)
-                    || bt(cursor + 1, y, x + 1)
-                    || bt(cursor + 1, y, x - 1)
-    
-                board[y][x] = tmp
-    
-                return result
-            }
-    
-    
-    
-            for (y in board.indices) {
-                for (x in board[y].indices) {
-                    if (bt(0, y, x)) {
-                        return true
-                    }
-                }
-            }
-            return false
-    
-        }
-        
-        val answers = mutableListOf<String>()
+        val root = TrieNode()
         for (word in words) {
-            if (exist(board, word)) {
-                answers.add(word)
+            var cursor = root
+
+            for (c in word.toCharArray()) {
+                if (!cursor.children.contains(c)) {
+                    cursor.children[c] = TrieNode()
+                }
+                cursor = cursor.children[c]!!
+            }
+
+            cursor.word = word
+        }
+
+        val dirY = intArrayOf(0, 1, 0, -1)
+        val dirX = intArrayOf(1, 0, -1, 0)
+
+        val n = board.size
+        val m = board[0].size
+
+        val answers = mutableListOf<String>()
+
+        fun bt(y: Int, x: Int, parentCursor: TrieNode) {
+            val currentChar = board[y][x]
+            val cursor = parentCursor.children[currentChar]!!
+
+            if (cursor.word.isNotEmpty()) { // found, remove it to block duplicated
+                answers.add(cursor.word)
+                cursor.word = ""
+            }
+
+            board[y][x] = '*' // marking
+
+            for (i in 0 until 4) {
+                val ny = y + dirY[i]
+                val nx = x + dirX[i]
+
+                if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+                    continue
+                }
+
+                if (cursor.children.contains(board[ny][nx])) {
+                    bt(ny, nx, cursor)
+                }
+            }
+
+            board[y][x] = currentChar // un-marking
+
+            if (cursor.children.isEmpty()) {
+                parentCursor.children.remove(currentChar) // 이미 이 하위의 자식들은 다 확인이 끝났으니 제거해버린다.
             }
         }
-        
+
+        for (y in 0 until n) {
+            for (x in 0 until m) {
+                val currentChar = board[y][x]
+                if (root.children.contains(currentChar)) {
+                    bt(y, x, root)
+                }
+            }
+        }
+
         return answers
     }
 }
+
