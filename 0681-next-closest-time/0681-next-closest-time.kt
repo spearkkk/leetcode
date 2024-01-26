8af800948abeb4c2679a9c66693f954ef706ef35
ChangKwon Jeong <spearkkp@gmail.com>
2024-01-26 20:30:14 +0900

    Time: 193 ms (54.55%), Space: 38.6 MB (45.45%) - LeetHub

--- 0681-next-closest-time/0681-next-closest-time.kt
+++ 0681-next-closest-time/0681-next-closest-time.kt
@@ -4,6 +4,7 @@ class Solution {
             return a.code - '0'.code
         }
 
+        // initialize numbers
         val nums = IntArray(4)
 
         nums[0] = aToi(time[0])
@@ -11,12 +12,16 @@ class Solution {
         nums[2] = aToi(time[3])
         nums[3] = aToi(time[4])
 
+        // to get current number 
         val numberTime = nums[0] * 1000 + nums[1] * 100 + nums[2] * 10 + nums[3]
 
+        // to sort candidates 
         val candi = sortedSetOf<Int>()
 
+        // backtracking.. to get all candidates
         fun bt(cnt: Int, num: Int) {
             if (cnt == 4) {
+                // validation for time
                 val hr = num / 100
                 val min = num % 100
                 if (hr in 0 until 24 && min in 0 until 60) {
@@ -33,8 +38,10 @@ class Solution {
         bt(0, 0)
 
         val sorted = candi.toList()
+        // get index to next close time
         val k = (sorted.withIndex().find { it.value == numberTime }!!.index + 1) % sorted.size
 
+        // return with formatting
         return "%02d:%02d".format(sorted[k] / 100, sorted[k] % 100)
     }
 }
