5b9405e1609ed650687c55a7696f7f44439429a3
ChangKwon Jeong <spearkkp@gmail.com>
2024-04-27 21:03:59 +0900

    Time: 1406 ms (6.67%), Space: 54.5 MB (15.56%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.kt
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.kt
@@ -4,45 +4,41 @@ class Solution {
      * 각 지점에서 가질 수 있는 직사각형의 넓이를 구하고 최솟값을 가지고 있으면 될까?
      * 한 지점에서 가장 먼저 만들어지는 직사각형이 최솟값이다.
      *
-     * 직사각형을 만들기 위해서는 반드시 3개의 지점을 알아야한다.
-     * 3개의 점을 알면 나머지 한 지점은 정해져 있다.
-     * 각 지점마다 만족하는 3개의 점을 찾으면 불필요하게 찾게 된다.
-     * 따라서 기준 지점에서 y 값은 같지만 x값이 증가하는 수 중에서 하나를 선택한다.
-     * 그리고 x 값은 같지만 y값이 증가하는 수 중에서 또 다른 하나를 선택한다.
+     * 직사각형을 만들기 위해 서로 같은 선에 위치하지 않은(수평/수직이 아닌) 두 개의 점을 선택한다.
+     * 두 점을 잇는 선을 대각선으로 하는 직사각형을 그린다면 나머지 두 개의 점도 찾을 수 있다.
+     * 나머지 두 점이 있을 때 면적을 계산할 수 있다.
+     *
      */
-    private val comparator = (compareBy<Pair<Int, Int>> { it.second }).then(compareBy { it.first })
     fun minAreaRect(points: Array<IntArray>): Int {
         val pts = mutableSetOf<Pair<Int, Int>>()
 
-        val pq = java.util.PriorityQueue<Pair<Int, Int>>(comparator)
-        val xToYs = Array<MutableList<Int>>(4 * 10_000 + 1) { mutableListOf() }
-        val yToXs = Array<MutableList<Int>>(4 * 10_000 + 1) { mutableListOf() }
+        val xs = mutableSetOf<Int>()
+        val ys = mutableSetOf<Int>()
 
         for (point in points) {
             val (x, y) = point
             val pt = Pair(x, y)
             pts.add(pt)
-            pq.add(pt)
-
-            xToYs[x].add(y)
-            yToXs[y].add(x)
+            xs.add(x)
+            ys.add(y)
         }
 
         var min = Long.MAX_VALUE
-        while (pq.isNotEmpty()) {
-            val (x, y) = pq.remove()
-//            println("x: $x, y: $y")
-            for (aX in yToXs[y]) {
-                for (aY in xToYs[x]) {
-                    if (aX <= x ||
-                        aY <= y ||
-                        !pts.contains(Pair(x, aY)) ||
-                        !pts.contains(Pair(aX, y)) ||
-                        !pts.contains(Pair(aX, aY))) {
-                        continue
+        for (x in xs) {
+            for (y in ys) {
+                for (aX in xs) {
+                    for (aY in ys) {
+                        if (aX == x || aY == y) {
+                            continue
+                        }
+                        if (pts.contains(Pair(x, y)) &&
+                                pts.contains(Pair(aX, aY)) &&
+                                pts.contains(Pair(aX, y)) &&
+                                pts.contains(Pair(x, aY))) {
+                            min = kotlin.math.min(min, kotlin.math.abs((aX - x) * (aY - y)).toLong())
+                        }
+
                     }
-//                    println("x: $x, y: $y, aX: $aY, aY: $aY, min: $min")
-                    min = kotlin.math.min(min, ((aX - x) * (aY - y)).toLong())
                 }
             }
         }
